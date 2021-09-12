b2d5eb2eb852985137fedfbf4ee4a464177113fb
Steven Ma <steven.malong@gmail.com>
2021-09-12 22:46:57 +1000

    Add more

--- CSharpDesignPattern/CSharpDesignPattern/Program.cs
+++ CSharpDesignPattern/CSharpDesignPattern/Program.cs
@@ -4,9 +4,80 @@ namespace CSharpDesignPattern
 {
 	class Program
 	{
-		static void Main(string[] args)
+        public abstract class Shape
+        {
+            private string Name { get; set; }
+            protected IRenderer Renderer;
+
+            public Shape(IRenderer renderer, string name)
+            {
+                Name = name;
+                Renderer = renderer;
+            }
+
+            public override string ToString() => Renderer.Draw(Name);
+        }
+
+        public class Triangle : Shape
+        {
+            public Triangle(IRenderer render) : base(render, "Triangle")
+            {
+            }
+        }
+
+        public class Square : Shape
+        {
+            public Square(IRenderer render) : base(render, "Square")
+            {
+            }
+        }
+
+        // imagine VectorTriangle and RasterTriangle are here too
+
+        public interface IRenderer
+        {
+            string Draw(string name);
+        }
+
+        public class RasterRenderer : IRenderer
+        {
+            public string Draw(string name)
+            {
+                return $"Drawing {name} as pixels";
+            }
+        }
+
+        public class VectorRenderer : IRenderer
+        {
+            public string Draw(string name)
+            {
+                return $"Drawing {name} as lines";
+            }
+        }
+
+        public class VectorSquare : Square
+        {
+            public VectorSquare() : base(new VectorRenderer())
+            {
+
+            }
+        }
+
+        public class RasterSquare : Square
+        {
+            public RasterSquare() : base(new RasterRenderer())
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-		}
+            var vectorSquare1 = new VectorSquare();
+            Console.WriteLine(vectorSquare1.ToString());
+
+            var rasterSquare1 = new RasterSquare();
+            Console.WriteLine(rasterSquare1.ToString());
+        }
 	}
 }
---
+++ CSharpDesignPattern/CompositePattern1/Program.cs
@@ -0,0 +1,76 @@
+﻿using CompositePattern1;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompositePattern1
+{
+    public interface IValueContainer : IEnumerable<int>
+    {
+
+    }
+
+    public class SingleValue : IValueContainer
+    {
+        public int Value;
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return Value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class ManyValues : List<int>, IValueContainer
+    {
+
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Sum(this List<IValueContainer> containers)
+        {
+            int result = 0;
+            foreach (var c in containers)
+                foreach (var i in c)
+                    result += i;
+            return result;
+        }
+    }
+
+    class Program
+	{
+        static void Main(string[] args)
+		{
+            
+            var singleValue = new SingleValue { Value = 1 };
+			var manyValues = new ManyValues { 2, 3, 4 };
+
+            var valueContainers = new List<IValueContainer> {singleValue, manyValues};
+			Console.WriteLine($"Result is \"{ExtensionMethods.Sum(valueContainers)}\"");
+		}
+	}
+}
+
+namespace Coding.Exercise.Tests
+{
+    [TestFixture]
+    public class TestSuite
+    {
+        [Test]
+        public void Test()
+        {
+            var singleValue = new SingleValue { Value = 11 };
+            var otherValues = new ManyValues();
+            otherValues.Add(22);
+            otherValues.Add(33);
+            Assert.That(new List<IValueContainer> { singleValue, otherValues }.Sum(),
+              Is.EqualTo(66));
+        }
+    }
+}
